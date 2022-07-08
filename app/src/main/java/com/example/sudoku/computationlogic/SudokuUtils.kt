27a86e4e39dd8135adcf378e890652a0a70fcc9c
A1venG0 <artemlev11@ukr.net>
2022-07-08 13:57:41 +0300

    Added: Configuration changes

--- app/src/main/java/com/example/sudoku/computationlogic/SudokuUtils.kt
+++ app/src/main/java/com/example/sudoku/computationlogic/SudokuUtils.kt
@@ -4,5 +4,6 @@ import com.example.sudoku.domain.GameStorageResult
 import com.example.sudoku.domain.SudokuPuzzle
 
 internal fun puzzleIsComplete(puzzle: SudokuPuzzle): Boolean {
-    TODO("not yet implemented")
+    // not yet implemented
+    return true
 }
